9341ef7820fe8542a5a0d6c1ed4b5585cd01202f
Rob Kian <rkndvr@gmail.com>
2025-06-14 18:53:36 +0800

    ayos commit

--- lexia_random_forest/settings.py
+++ lexia_random_forest/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://lexiaweb.io",  # already present
+    "https://lexia-random-forest.onrender.com/"
+]
+
